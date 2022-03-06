9eea595bce72cee5fc25bd1baec9aa2c9108b6ab
qdwds <52602678+qdwds@users.noreply.github.com>
2022-03-06 21:34:39 +0800

    w2_2 校验修正

--- w2_2/contracts/Score.sol
+++ w2_2/contracts/Score.sol
@@ -12,26 +12,27 @@ interface IScore{
 
 contract Score is IScore{
     mapping(address => uint8) public scores;
-    address public teacher;
+    address public conAddr;
 
-    constructor(address _teacher){
-        teacher = _teacher;
+    constructor(address _conAddr){
+        conAddr = _conAddr;
     }
-    modifier _isTeacher {
-        require(msg.sender == teacher,"You a Teacher ?");
+    //  校验合约
+    modifier _isTeacherContract {
+        require(msg.sender == conAddr,"contract ok ?");
         _;
     }
-    function increment(address _addr, uint8 _val) external {
+    function increment(address _addr, uint8 _val) external _isTeacherContract{
         require(scores[msg.sender] + _val <= 100, "Cannot exceed 100 !");
         scores[_addr] += _val;
     }
 
-    function decrement(address _addr, uint8 _val) external {
+    function decrement(address _addr, uint8 _val) external _isTeacherContract{
         require(_val <= 100 && _val >= 0, "number ?");
         scores[_addr] -= _val;
     }
 
-    function setScore(address _addr, uint8 _val) external {
+    function setScore(address _addr, uint8 _val) external _isTeacherContract{
         require(scores[_addr] <= 100, "Cannot exceed 100 !");
         scores[_addr] = _val;
     }
@@ -43,16 +44,16 @@ contract Teacher{
     IScore score;
     address public teacher;
 
-    
+     event asd(address aa,address addres);
     constructor(){
         teacher = msg.sender;
-        score = new Score(teacher);
+        score = new Score(address(this));
     }
+    //  校验调用者
     modifier _isTeacher{
         require(msg.sender == teacher,"Is teacher ?");
         _;
     }
-
     
     function increment(address _addr, uint8 _val) public _isTeacher  {
         score.increment(_addr, _val);
